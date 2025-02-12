6bed0e6c1ef584b9e82746d2fa40da1ba18be374
Simon Masson <simon.masson@yahoo.fr>
2025-02-12 18:01:28 +0100

    falcon recovery works

---
+++ python-ref/falcon_recovery.py
@@ -0,0 +1,100 @@
+"""Python implementation of Recovery Falcon"""
+from common import q
+from encoding import compress, decompress
+from falcon import HEAD_LEN, SALT_LEN, SEED_LEN, SecretKey, logn
+from keccak_prng import KeccakPRNG
+from polyntt.poly import Poly
+from Crypto.Hash import keccak
+# Randomness
+from os import urandom
+
+
+class RecoveryModeSecretKey(SecretKey):
+    def __init__(self, n, polys=None, ntt='NTTIterative'):
+        super().__init__(n, polys, ntt)
+        keccak_ctx = keccak.new(digest_bytes=32)
+        keccak_ctx.update(b''.join(x.to_bytes(3, 'big') for x in self.h))
+        self.pk = keccak_ctx.digest()
+
+    def sign_with_recovery(self, message, randombytes=urandom, xof=KeccakPRNG):
+        """
+        Sign a message. The message MUST be a byte string or byte array.
+        Optionally, one can select the source of (pseudo-)randomness used
+        (default: urandom).
+        """
+        int_header = 0x30 + logn[self.n]
+        header = int_header.to_bytes(1, "little")
+
+        salt = randombytes(SALT_LEN)
+        hashed = self.hash_to_point(message, salt, xof=xof)
+
+        # We repeat the signing procedure until we find a signature that is
+        # short enough (both the Euclidean norm and the bytelength)
+        while (1):
+            if (randombytes == urandom):
+                s = self.sample_preimage(hashed)
+            else:
+                seed = randombytes(SEED_LEN)
+                s = self.sample_preimage(hashed, seed=seed)
+            # We need s1 to be invertible
+            # TODO: is it secure to do so?
+            s_1_ntt = Poly(s[1], q).ntt()
+            if all(elt % q != 0 for elt in s_1_ntt):
+                norm_sign = sum(coef ** 2 for coef in s[0])
+                norm_sign += sum(coef ** 2 for coef in s[1])
+                # Check the Euclidean norm
+                if norm_sign <= self.signature_bound:
+                    # We compress here s[0] and s[1], not s_1_inv_ntt.
+                    enc_s = compress(
+                        s[0]+s[1], self.sig_bytelen * 2 - HEAD_LEN - SALT_LEN)
+                    s_1_inv = Poly(s[1], q).inverse().coeffs
+                    # 3 * n bytes required for s1_inv
+                    bytes_s1_inv = b''.join(x.to_bytes(3, 'big')
+                                            for x in s_1_inv)
+                    # Check that the encoding is valid (sometimes it fails)
+                    if enc_s is not False:
+                        return header + salt + enc_s + bytes_s1_inv
+
+    def verify_with_recovery(self, message, signature, ntt='NTTIterative', xof=KeccakPRNG):
+        """
+        Verify a signature.
+        """
+        # Unpack the salt and the short polynomial s1
+        salt = signature[HEAD_LEN:HEAD_LEN + SALT_LEN]
+        enc_s = signature[HEAD_LEN+SALT_LEN:-self.n*3]
+        s = decompress(enc_s, self.sig_bytelen * 2 -
+                       HEAD_LEN - SALT_LEN, self.n*2)
+        # Check that the encoding is valid
+        if (s is False):
+            print("Invalid encoding")
+            return False
+        mid = len(s)//2
+        s0, s1 = s[:mid], s[mid:]
+        # s_1_inv
+        byte_s_1_inv = signature[-self.n*3:]
+        s_1_inv = Poly([int.from_bytes(byte_s_1_inv[i:i+3], 'big')
+                        for i in range(0, len(byte_s_1_inv), 3)], q)
+
+        # check that s_1_inv * s_1 == 1
+        if (s_1_inv * Poly(s1, q)).coeffs != [1] + [0 for i in range(len(s1)-1)]:
+            return False
+
+        # Check that the (s0, s1) is short
+        norm_sign = sum(coef ** 2 for coef in s0)
+        norm_sign += sum(coef ** 2 for coef in s1)
+        if norm_sign > self.signature_bound:
+            print("Squared norm of signature is too large:", norm_sign)
+            return False
+
+        # Compute s0 and normalize its coefficients in (-q/2, q/2]
+        hashed = Poly(self.hash_to_point(message, salt, xof=xof), q, ntt=ntt)
+        s0 = Poly(s0, q, ntt=ntt)
+        # recover h
+        h = (hashed - s0) * s_1_inv
+        bytes_h = b''.join(x.to_bytes(3, 'big') for x in h.coeffs)
+        keccak_ctx = keccak.new(digest_bytes=32)
+        keccak_ctx.update(bytes_h)
+        if self.pk != keccak_ctx.digest():
+            return False
+        # If all checks are passed, accept
+        return True
--- python-ref/test_recovery_mode.py
+++ python-ref/test_recovery_mode.py
@@ -1,8 +1,8 @@
 import unittest
 
-from falcon import PublicKey, SecretKey, RecoveryModeSecretKey
+from falcon_recovery import RecoveryModeSecretKey
 from scripts.sign_KAT import sign_KAT
-from keccaxof import KeccaXOF
+from keccak_prng import KeccakPRNG
 
 
 class TestRecoveryMode(unittest.TestCase):
@@ -17,13 +17,13 @@ class TestRecoveryMode(unittest.TestCase):
         sig = sk.sign_with_recovery(message)
         self.assertTrue(sk.verify_with_recovery(message, sig))
 
-    def test_signature_recovery_mode_keccaxof(self):
+    def test_signature_recovery_mode_keccak_prng(self):
         n = 512
         f = sign_KAT[n][0]["f"]
         g = sign_KAT[n][0]["g"]
         F = sign_KAT[n][0]["F"]
         G = sign_KAT[n][0]["G"]
-        sk = RecoveryModeSecretKey(n, [f, g, F, G], xof=KeccaXOF)
+        sk = RecoveryModeSecretKey(n, [f, g, F, G])
         message = b"abc"
-        sig = sk.sign_with_recovery(message, xof=KeccaXOF)
-        self.assertTrue(sk.verify_with_recovery(message, sig, xof=KeccaXOF))
+        sig = sk.sign_with_recovery(message, xof=KeccakPRNG)
+        self.assertTrue(sk.verify_with_recovery(message, sig, xof=KeccakPRNG))
