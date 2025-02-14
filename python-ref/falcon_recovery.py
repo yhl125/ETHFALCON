c4a364136c352213252aa0da1e7bd16632ccd60c
Simon Masson <simon.masson@yahoo.fr>
2025-02-14 12:51:37 +0100

    FalconRec matching the spec

--- python-ref/falcon_recovery.py
+++ python-ref/falcon_recovery.py
@@ -4,6 +4,7 @@ from encoding import compress, decompress
 from falcon import HEAD_LEN, SALT_LEN, SEED_LEN, SecretKey, logn
 from keccak_prng import KeccakPRNG
 from polyntt.poly import Poly
+from polyntt.ntt_iterative import NTTIterative
 from Crypto.Hash import keccak
 # Randomness
 from os import urandom
@@ -49,7 +50,12 @@ class RecoveryModeSecretKey(SecretKey):
                         s[0]+s[1], self.sig_bytelen * 2 - HEAD_LEN - SALT_LEN)
                     # Check that the encoding is valid (sometimes it fails)
                     if enc_s is not False:
-                        return header + salt + enc_s
+                        # TODO could be done more efficiently with vec_inv in ntt domain
+                        s_1_inv_ntt = Poly(s[1], q).inverse().ntt()
+                        # 3 * n bytes required for s1_inv
+                        bytes_s1_inv_ntt = b''.join(x.to_bytes(3, 'big')
+                                                    for x in s_1_inv_ntt)
+                        return header + salt + enc_s + bytes_s1_inv_ntt
 
     def verify(self, message, signature, ntt='NTTIterative', xof=KeccakPRNG):
         """
@@ -57,7 +63,7 @@ class RecoveryModeSecretKey(SecretKey):
         """
         # Unpack the salt and the short polynomial s1
         salt = signature[HEAD_LEN:HEAD_LEN + SALT_LEN]
-        enc_s = signature[HEAD_LEN+SALT_LEN:]  # -self.n*3]
+        enc_s = signature[HEAD_LEN+SALT_LEN: -self.n*3]
         s = decompress(enc_s, self.sig_bytelen * 2 -
                        HEAD_LEN - SALT_LEN, self.n*2)
         # Check that the encoding is valid
@@ -66,7 +72,18 @@ class RecoveryModeSecretKey(SecretKey):
             return False
         mid = len(s)//2
         s0, s1 = s[:mid], s[mid:]
-        s_1_inv = Poly(s1, q).inverse()
+
+        # s_1_inv
+        byte_s_1_inv_ntt = signature[-self.n*3:]
+        s_1_inv_ntt = [int.from_bytes(byte_s_1_inv_ntt[i:i+3], 'big')
+                       for i in range(0, len(byte_s_1_inv_ntt), 3)]
+        T = NTTIterative(q)
+        s_1_ntt = T.ntt(s1)
+        # check that s_1_inv_ntt * s_1_ntt == [1, ... , 1]
+        mul_s1_s1inv = T.vec_mul(s_1_inv_ntt, s_1_ntt)
+        for elt in mul_s1_s1inv:
+            if elt != 1:
+                return False
 
         # Check that the (s0, s1) is short
         norm_sign = sum(coef ** 2 for coef in s0)
@@ -79,7 +96,7 @@ class RecoveryModeSecretKey(SecretKey):
         hashed = Poly(self.hash_to_point(message, salt, xof=xof), q, ntt=ntt)
         s0 = Poly(s0, q, ntt=ntt)
         # recover h
-        h = (hashed - s0) * s_1_inv
+        h = (hashed - s0).mul_opt(s_1_inv_ntt)
         bytes_h = b''.join(x.to_bytes(3, 'big') for x in h.coeffs)
         keccak_ctx = keccak.new(digest_bytes=32)
         keccak_ctx.update(bytes_h)
