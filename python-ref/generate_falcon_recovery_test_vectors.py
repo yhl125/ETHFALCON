56028eb293c9b60f00c76f98babbe06979f693ff
Simon Masson <simon.masson@yahoo.fr>
2025-02-14 15:34:19 +0100

    TestVectors for Falcon work now

--- python-ref/generate_falcon_recovery_test_vectors.py
+++ python-ref/generate_falcon_recovery_test_vectors.py
@@ -1,8 +1,7 @@
 import hashlib
-from falcon import HEAD_LEN, SALT_LEN, Params, decompress, SecretKey, PublicKey
+from falcon_recovery import RecoveryModeSecretKey, HEAD_LEN, SALT_LEN, decompress
 from polyntt.poly import Poly
 from common import q
-from scripts.sign_KAT import sign_KAT
 from keccaxof import KeccaXOF
 
 
@@ -26,8 +25,7 @@ F = [-23, 22, -11, -5, -18, 61, -2, 3, 1, -4, 12, -5, 3, -35, -49, 4, 14, -18, -
 G = [-10, 12, -13, -20, 7, 32, -17, 31, -61, -3, 23, -65, 28, -61, -22, 56, 33, 11, 12, 7, 34, -33, 21, 27, -22, 67, -14, -5, -18, -4, -1, -42, 3, 51, -19, -1, 16, 14, 55, -37, 29, 28, 32, 20, -31, -46, 39, 7, -1, -22, -61, 23, -22, 9, -13, 11, 20, 16, -30, 1, 52, 46, 10, -6, 12, -31, 31, -15, 9, -19, -7, -5, 5, 16, -19, 63, -35, -21, -22, 0, 46, 7, -16, -12, 5, 3, -8, -43, 21, 21, 47, -30, -29, 16, 9, 14, -21, -9, 8, -26, -9, 20, -42, 59, -14, 6, 20, -31, -37, 14, 39, 0, 2, -40, 43, -3, -32, -27, -21, -15, 2, 0, -23, -5, -3, -9, 12, -27, -36, 4, -22, 23, -1, -12, 32, -39, -4, -33, -20, -7, -12, -22, 35, -21, 4, 2, 8, 26, -24, 12, -2, -39, -3, 8, 59, 12, -16, -8, -5, 36, 11, 16, 45, 19, -50, -32, -7, 27, 3, -17, 52, -27, -64, -12, -9, 6, -13, -29, -10, 2, 44, -16, -12, -9, -19, -6, -6, 6, 24, -55, 42, 38, -15, 38, -16, -22, -4, -9, 38, 21, 12, 50, -11, 9, 31, -33, 46, -12, 40, 2, -15, 6, -41, 27, 44, -1, -10, -9, -17, 24, 4, -5, 2, -22, 32, -51, -2, -23, 18, 27, -15, -17, 34, -15, 13, 1, -5, 36, 3, 36, -7, -8, 33, -19, -14, 4, -7, -8, 15, 0, -32, -7, -31,
      67, -27, -35, -6, 26, 13, -12, 24, 35, -21, 24, -16, 16, 10, 47, -14, 3, -5, 2, 3, -26, 0, -29, 4, 21, -17, -16, -20, 7, -44, -34, 26, 2, 6, -8, -17, 17, -14, 7, -5, 6, -33, 13, 6, 35, 21, -42, 3, 5, 8, 23, 27, -10, -40, 4, -20, 9, -31, -40, 14, 9, 45, -12, -32, 4, 7, 15, 25, 7, 9, 23, 4, 33, -35, 47, 2, 30, -22, 8, -38, -28, 62, -16, 13, -4, 5, 16, 34, -8, 44, 26, -45, 27, -42, 26, 33, -22, -25, 0, -3, -29, 6, 18, 11, 4, 9, -20, -9, 1, 14, -8, -6, -34, -11, -26, -2, -10, 35, -1, -24, 17, 4, 3, 76, -18, -13, 4, 19, 4, -41, 8, -17, -31, -4, -27, 24, -14, -1, 41, -7, -38, 27, 24, 12, 1, -25, 22, 10, -28, 25, 7, 29, -19, 9, 20, 5, -17, -24, 38, 0, 18, -23, 6, -30, -9, -38, -21, -32, 16, -5, 16, 1, -24, -17, 17, 34, -39, -25, -16, 26, 13, -18, -11, -8, -46, 27, 14, -27, -22, -22, -1, -41, -5, 11, -2, 57, 1, -16, -30, 25, 46, -20, 2, 9, 25, -30, 18, 39, -9, -53, 30, 14, 24, -22, 29, -8, 0, -18, 22, -2, -11, -35, -12, 24, 9, -20, -17, -39, 2, -3, -36, 31, -23, -4, 22, -40, 4, 0, 23, 26, -7, -8, 12, -31, -32, -10, -18, 24, 17, 0, 63, -29, 67, 44, 3, 13, 35, 11, -36]
 
-sk = SecretKey(n, [f, g, F, G])
-pub = PublicKey(sk)
+sk = RecoveryModeSecretKey(n, [f, g, F, G])
 
 header = """
 // code generated using pythonref/generate_signature_test_vectors.py.
@@ -50,10 +48,12 @@ for (i, message) in enumerate(["My name is Renaud", "My name is Simon", "My name
     sig = sk.sign(message.encode(),
                   randombytes=deterministic_salt, xof=KeccaXOF)
     salt = sig[HEAD_LEN:HEAD_LEN + SALT_LEN]
-    enc_s = sig[HEAD_LEN + SALT_LEN:]
-    s1 = decompress(enc_s, sk.sig_bytelen - HEAD_LEN - SALT_LEN, sk.n)
-    s1 = [elt % q for elt in s1]
-    s1_inv = Poly(s1, q).inverse().coeffs
+    enc_s = sig[HEAD_LEN + SALT_LEN:-sk.n*3]
+    s = decompress(enc_s, sk.sig_bytelen*2 - HEAD_LEN - SALT_LEN, sk.n*2)
+    mid = len(s)//2
+    s = [elt % q for elt in s]
+    s0, s1 = s[:mid], s[mid:]
+    s1_inv_ntt = Poly(s1, q).inverse().ntt()
     h = sk.hash_to_point(salt, message.encode())
     h_ntt = Poly(h, q).ntt()
     assert sk.verify(message.encode(), sig, xof=KeccaXOF)
@@ -68,20 +68,24 @@ for (i, message) in enumerate(["My name is Renaud", "My name is Simon", "My name
     file.write("\tpk[i] = tmp_pk[i];\n")
     file.write("}\n")
 
-    file.write("// signature s1\n")
+    file.write("// signature s0\n")
+    file.write("// forgefmt: disable-next-line\n")
+    file.write("int[512] memory tmp_s0 = [int({}), {}];\n".format(
+        s1[0], ','.join(map(str, s1[1:]))))
     file.write("// forgefmt: disable-next-line\n")
     file.write("int[512] memory tmp_s1 = [int({}), {}];\n".format(
         s1[0], ','.join(map(str, s1[1:]))))
     file.write("ETHFalcon.Signature memory sig;\n")
-    file.write("sig.s1 = new int256[](512);\n")
+    file.write("sig.s0 = new int256[](512);\n")
     file.write("for (uint i = 0; i < 512; i++) {\n")
+    file.write("\tsig.s0[i] = tmp_s0[i];\n")
     file.write("\tsig.s1[i] = tmp_s1[i];\n")
     file.write("}\n")
 
     file.write("// signature s1 inverse\n")
     file.write("// forgefmt: disable-next-line\n")
-    file.write("int[512] memory tmp_s1_inv = [int({}), {}];\n".format(
-        s1_inv[0], ','.join(map(str, s1_inv[1:]))))
+    file.write("int[512] memory tmp_s1_inv_ntt = [int({}), {}];\n".format(
+        s1_inv_ntt[0], ','.join(map(str, s1_inv_ntt[1:]))))
 
     file.write("// message\n")
     file.write("bytes memory message  = \"{}\"; \n".format(message))
