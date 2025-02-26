7d2650b93aa8bc7ff3ebbd3d4308fa94c3a6cd4c
Simon Masson <simon.masson@yahoo.fr>
2025-02-26 18:05:19 +0100

    better hashtopoint

--- src/HashToPoint.sol
+++ src/HashToPoint.sol
@@ -40,6 +40,42 @@ pragma solidity ^0.8.25;
 
 import {Test, console} from "forge-std/Test.sol";
 
+uint256 constant MASK_2BYTES = uint256(0xFFFF);
+
+function hashToPointZKNOX(bytes memory salt, bytes memory msgHash, uint256 q, uint256 n)
+    pure
+    returns (uint256[] memory output)
+{
+    output = new uint256[](n);
+    uint256 kq = ((1 << 16) / q) * q;
+    bytes32 state;
+    bytes32 buffer;
+
+    // Initial state
+    state = keccak256(abi.encodePacked(msgHash, salt));
+    uint256 j;
+
+    uint64 counter = 0;
+    uint256 i = 0;
+    while (i < n) {
+        // updating buffer = keccak(state||counter)
+        buffer = keccak256(abi.encodePacked(state, counter));
+
+        for (j = 0; j < 16; j++) {
+            // consider slices of two bytes
+            if (i >= n) break;
+            // MSB (for LSB, simply replace (15 - j) by j)
+            uint256 chunk = uint256(buffer >> (16 * (15 - j))) & MASK_2BYTES;
+            // rejection sampling before modular reduction
+            if (chunk < kq) {
+                output[i] = chunk % q;
+                i += 1;
+            }
+        }
+        counter += 1;
+    }
+}
+
 function splitToHex(bytes32 x) pure returns (uint16[16] memory) {
     // splits a byte32 into hex
     uint16[16] memory res;
@@ -47,68 +83,6 @@ function splitToHex(bytes32 x) pure returns (uint16[16] memory) {
         res[i] = uint16(uint256(x) >> ((15 - i) * 16));
     }
     return res;
-}
-
-function hashToPointZKNOX(bytes memory salt, bytes memory msgHash, uint256 q, uint256 n)
-    pure
-    returns (uint256[] memory c)
-{
-    uint256 k = (1 << 16) / q;
-    bytes32 state;
-    uint64 counter;
-    bytes memory buffer;
-    bytes32 outBuffer;
-    uint8 outBufferPos;
-    bool outBufferValid;
-
-    counter = 0;
-    outBufferPos = 0;
-    outBufferValid = false;
-
-    // Inject
-    buffer = abi.encodePacked("", abi.encodePacked(msgHash, salt));
-    // Flip
-    state = keccak256(buffer);
-
-    uint256 kq = k * q;
-    uint256 t;
-    c = new uint256[](512);
-    uint256 i = 0;
-    while (i < n) {
-        assembly {
-            mstore(add(buffer, 32), 0)
-        }
-
-        buffer = new bytes(2);
-        uint256 offset = 0;
-        // Use any remaining bytes in the output buffer first
-        while (outBufferValid && outBufferPos < 32 && offset < 2) {
-            buffer[offset] = outBuffer[outBufferPos];
-            outBufferPos++;
-            offset++;
-        }
-        // Generate two blocks
-        while (offset < 2) {
-            outBuffer = keccak256(abi.encodePacked(state, abi.encodePacked(counter)));
-            outBufferPos = 0;
-            outBufferValid = true;
-            while (outBufferPos < 32 && offset < 2) {
-                buffer[offset] = outBuffer[outBufferPos];
-                outBufferPos++;
-                offset++;
-            }
-            counter++;
-        }
-
-        assembly {
-            t := mload(add(buffer, 32))
-        }
-        t = t >> (256 - 16);
-        if (t < kq) {
-            c[i] = t % q;
-            i++;
-        }
-    }
 }
 
 //Use for Poc only, as this XOF doesn't respect separation domain for input and output of internal state
