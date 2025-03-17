47c3ebec8cc45276d4d013815da82132d2a81a82
renaud dubois <renaud.dubois.fr@gmail.com>
2025-03-17 19:17:47 +0100

    WIP shake

--- src/ZKNOX_shake.sol
+++ src/ZKNOX_shake.sol
@@ -1,49 +1,63 @@
-// SPDX-License-Identifier: UNLICENSED
+/**
+ *
+ */
+/*ZZZZZZZZZZZZZZZZZZZKKKKKKKKK    KKKKKKKNNNNNNNN        NNNNNNNN     OOOOOOOOO     XXXXXXX       XXXXXXX                         ..../&@&#.       .###%@@@#, ..                         
+/*Z:::::::::::::::::ZK:::::::K    K:::::KN:::::::N       N::::::N   OO:::::::::OO   X:::::X       X:::::X                      ...(@@* .... .           &#//%@@&,.                       
+/*Z:::::::::::::::::ZK:::::::K    K:::::KN::::::::N      N::::::N OO:::::::::::::OO X:::::X       X:::::X                    ..*@@.........              .@#%%(%&@&..                    
+/*Z:::ZZZZZZZZ:::::Z K:::::::K   K::::::KN:::::::::N     N::::::NO:::::::OOO:::::::OX::::::X     X::::::X                   .*@( ........ .  .&@@@@.      .@%%%%%#&@@.                  
+/*ZZZZZ     Z:::::Z  KK::::::K  K:::::KKKN::::::::::N    N::::::NO::::::O   O::::::OXXX:::::X   X::::::XX                ...&@ ......... .  &.     .@      /@%%%%%%&@@#                  
+/*        Z:::::Z      K:::::K K:::::K   N:::::::::::N   N::::::NO:::::O     O:::::O   X:::::X X:::::X                   ..@( .......... .  &.     ,&      /@%%%%&&&&@@@.              
+/*       Z:::::Z       K::::::K:::::K    N:::::::N::::N  N::::::NO:::::O     O:::::O    X:::::X:::::X                   ..&% ...........     .@%(#@#      ,@%%%%&&&&&@@@%.               
+/*      Z:::::Z        K:::::::::::K     N::::::N N::::N N::::::NO:::::O     O:::::O     X:::::::::X                   ..,@ ............                 *@%%%&%&&&&&&@@@.               
+/*     Z:::::Z         K:::::::::::K     N::::::N  N::::N:::::::NO:::::O     O:::::O     X:::::::::X                  ..(@ .............             ,#@&&&&&&&&&&&&@@@@*               
+/*    Z:::::Z          K::::::K:::::K    N::::::N   N:::::::::::NO:::::O     O:::::O    X:::::X:::::X                   .*@..............  . ..,(%&@@&&&&&&&&&&&&&&&&@@@@,               
+/*   Z:::::Z           K:::::K K:::::K   N::::::N    N::::::::::NO:::::O     O:::::O   X:::::X X:::::X                 ...&#............. *@@&&&&&&&&&&&&&&&&&&&&@@&@@@@&                
+/*ZZZ:::::Z     ZZZZZKK::::::K  K:::::KKKN::::::N     N:::::::::NO::::::O   O::::::OXXX:::::X   X::::::XX               ...@/.......... *@@@@. ,@@.  &@&&&&&&@@@@@@@@@@@.               
+/*Z::::::ZZZZZZZZ:::ZK:::::::K   K::::::KN::::::N      N::::::::NO:::::::OOO:::::::OX::::::X     X::::::X               ....&#..........@@@, *@@&&&@% .@@@@@@@@@@@@@@@&                  
+/*Z:::::::::::::::::ZK:::::::K    K:::::KN::::::N       N:::::::N OO:::::::::::::OO X:::::X       X:::::X                ....*@.,......,@@@...@@@@@@&..%@@@@@@@@@@@@@/                   
+/*Z:::::::::::::::::ZK:::::::K    K:::::KN::::::N        N::::::N   OO:::::::::OO   X:::::X       X:::::X                   ...*@,,.....%@@@,.........%@@@@@@@@@@@@(                     
+/*ZZZZZZZZZZZZZZZZZZZKKKKKKKKK    KKKKKKKNNNNNNNN         NNNNNNN     OOOOOOOOO     XXXXXXX       XXXXXXX                      ...&@,....*@@@@@ ..,@@@@@@@@@@@@@&.                     
+/*                                                                                                                                   ....,(&@@&..,,,/@&#*. .                             
+/*                                                                                                                                    ......(&.,.,,/&@,.                                
+/*                                                                                                                                      .....,%*.,*@%                               
+/*                                                                                                                                    .#@@@&(&@*,,*@@%,..                               
+/*                                                                                                                                    .##,,,**$.,,*@@@@@%.                               
+/*                                                                                                                                     *(%%&&@(,,**@@@@@&                              
+/*                                                                                                                                      . .  .#@((@@(*,**                                
+/*                                                                                                                                             . (*. .                                   
+/*                                                                                                                                              .*/
+///* Copyright (C) 2025 - Renaud Dubois, Simon Masson - This file is part of ZKNOX project
+///* License: This software is licensed under MIT License
+///* This Code may be reused including this header, license and copyright notice.
+///* See LICENSE file at the root folder of the project.
+///* FILE: ZKNOX_falcon.sol
+///* Description: shake XOF function implementation
+/**
+ *
+ */
+// SPDX-License-Identifier: MIT
 //this is a direct translation from https://github.com/coruus/py-keccak/blob/master/fips202/keccak.py
 pragma solidity ^0.8.25;
 
 contract ZKNOX_shake {
-    uint256[24] _KECCAK_RHO =
-        [1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 2, 14, 27, 41, 56, 8, 25, 43, 62, 18, 39, 61, 20, 44];
-
-    uint256[24] _KECCAK_PI = [10, 7, 11, 17, 18, 3, 5, 16, 8, 21, 24, 4, 15, 23, 19, 13, 12, 2, 20, 14, 22, 9, 6, 1];
-
-    uint64[24] _KECCAK_RC = [
-        0x0000000000000001,
-        0x0000000000008082,
-        0x800000000000808a,
-        0x8000000080008000,
-        0x000000000000808b,
-        0x0000000080000001,
-        0x8000000080008081,
-        0x8000000000008009,
-        0x000000000000008a,
-        0x0000000000000088,
-        0x0000000080008009,
-        0x000000008000000a,
-        0x000000008000808b,
-        0x800000000000008b,
-        0x8000000000008089,
-        0x8000000000008003,
-        0x8000000000008002,
-        0x8000000000000080,
-        0x000000000000800a,
-        0x800000008000000a,
-        0x8000000080008081,
-        0x8000000000008080,
-        0x0000000080000001,
-        0x8000000080008008
-    ];
-
-    uint256 constant _SPONGE_ABSORBING = 1;
-    uint256 _SPONGE_SQUEEZING = 2;
-
-    // """Rotate uint64 x left by s.""
+  
+    uint constant _RATE=136;
+ 
+   // """Rotate uint64 x left by s.""
     function rol64(uint256 x, uint256 s) public pure returns (uint64) {
         return (uint64)(x << s ^ (x >> (64 - s)));
     }
 
-    function F1600(uint64[25] memory state) public view returns (uint64[25] memory) {
+   
+    function F1600(uint64[25] memory state) public pure returns (uint64[25] memory) {
+
+    // forgefmt: disable-next-line
+    uint256[24] memory _KECCAK_PI = [uint256(10), 7, 11, 17, 18, 3, 5, 16, 8, 21, 24, 4, 15, 23, 19, 13, 12, 2, 20, 14, 22, 9, 6, 1];
+    // forgefmt: disable-next-line
+    uint64[24] memory _KECCAK_RC = [uint64(0x0000000000000001), 0x0000000000008082,0x800000000000808a,0x8000000080008000,0x000000000000808b, 0x0000000080000001,0x8000000080008081, 0x8000000000008009,0x000000000000008a, 0x0000000000000088,0x0000000080008009, 0x000000008000000a,0x000000008000808b, 0x800000000000008b,0x8000000000008089, 0x8000000000008003,0x8000000000008002, 0x8000000000000080,0x000000000000800a, 0x800000008000000a,0x8000000080008081, 0x8000000000008080, 0x0000000080000001, 0x8000000080008008];
+     // forgefmt: disable-next-line
+     uint256[24] memory _KECCAK_RHO =[uint256(1), 3, 6, 10, 15, 21, 28, 36, 45, 55, 2, 14, 27, 41, 56, 8, 25, 43, 62, 18, 39, 61, 20, 44];
+
         uint64[5] memory bc = [uint64(0), 0, 0, 0, 0];
 
         for (uint256 i = 0; i < 24; i++) {
@@ -61,7 +75,7 @@ contract ZKNOX_shake {
             //# Theta
             for (uint256 x = 0; x < 5; x++) {
                 //range(5):
-                t = bc[(x + 4) % 5] ^ rol64(bc[(x + 1) % 5], 1);
+                t = bc[addmod(x,4,5)] ^ rol64(bc[addmod(x,1,5)], 1);
                 for (uint64 y = 0; y < 25; y += 5) {
                     // in range(0, 25, 5):
                     state[y + x] ^= t;
@@ -84,11 +98,93 @@ contract ZKNOX_shake {
                 }
                 for (uint256 x = 0; x < 5; x++) {
                     //range(5):
-                    state[y + x] = bc[x] ^ ((bc[(x + 1) % 5] ^ 0xffffffffffffffff) & bc[(x + 2) % 5]);
+                    state[y + x] = bc[x] ^ ((bc[addmod(x,1,5)] ^ 0xffffffffffffffff) & bc[addmod(x,2,5)]);
                 }
                 state[0] ^= _KECCAK_RC[i];
             }
         } //end loop i
         return state;
     } //end F1600
+
+    function absorb(uint256 i, uint8[200] memory buf, uint64[25] memory state, bytes memory input) public pure returns(uint8[200] memory bufout,uint64[25] memory stateout) {
+        uint256 todo = input.length;
+        uint256 index = 0;
+        while (todo > 0) {
+            uint256 cando = _RATE - i;
+            uint256 willabsorb = (cando < todo) ? cando : todo;
+            for (uint256 j = 0; j < willabsorb; j++) {
+                buf[i + j] ^= uint8(input[index + j]);
+            }
+            i += willabsorb;
+
+            for(i=0;i<200;i++){
+                state[i/8]^=buf[i]<<uint8(i&0x7); 
+            }
+
+            if (i == _RATE) {
+                state=permute(buf, state);
+                for(uint256 j=0; j<200; j++){
+                    buf[j]=0;
+                }
+            }
+            todo -= willabsorb;
+            index += willabsorb;
+        }
+
+        return (buf, state);
+    }
+
+    function update(bool _SPONGE_ABSORBING,uint256 i, uint8[200] memory buf, uint64[25] memory state, bytes memory input ) public pure returns(uint64[25] memory stateout) {
+        if(_SPONGE_ABSORBING==false){
+            state=permute(buf, state);
+        }
+        absorb(i, buf, state, input);
+
+    }
+
+ function squeeze(uint256 i, uint8[200] memory buf, uint64[25] memory state, uint256 n) public pure returns (bytes memory) {
+        bytes memory output = new bytes(n);
+        uint256 tosqueeze = n;
+        uint256 index = 0;
+        while (tosqueeze > 0) {
+            uint256 cansqueeze = _RATE - i;
+            uint256 willsqueeze = (cansqueeze < tosqueeze) ? cansqueeze : tosqueeze;
+            for (uint256 j = 0; j < willsqueeze; j++) {
+                output[index + j] = bytes1(uint8(state[i + j]));
+            }
+            i += willsqueeze;
+            if (i == _RATE) {
+                permute(buf, state);
+            }
+            tosqueeze -= willsqueeze;
+            index += willsqueeze;
+        }
+        return output;
+    }
+
+ function permute(uint8[200] memory buf, uint64[25] memory state) internal pure returns (uint64[25] memory stateout){
+        for (uint256 j = 0; j < 25; j++) {
+            state[j] ^= uint64(uint8(buf[j]));
+        }
+        // Call F1600 Keccak permutation function here
+        // KeccakF1600();
+       
+        for (uint256 j = 0; j < 200; j++) {
+            buf[j] = 0;
+        }
+
+
+
+        return state;//zeroization of buf external to this function
+    }
+
+
+    //to be yuled
+  function pad(uint256 i, uint8[200] memory buf) internal view returns(uint8[200] memory bufout) {
+        buf[i] ^= 0x1f;
+        buf[_RATE - 1] ^= 0x80;
+   //     F1600(buf);
+    }
+
+
 }
--- test/ZKNOX_shake.t.sol
+++ test/ZKNOX_shake.t.sol
@@ -1,9 +1,48 @@
+/**
+ *
+ */
+/*ZZZZZZZZZZZZZZZZZZZKKKKKKKKK    KKKKKKKNNNNNNNN        NNNNNNNN     OOOOOOOOO     XXXXXXX       XXXXXXX                         ..../&@&#.       .###%@@@#, ..                         
+/*Z:::::::::::::::::ZK:::::::K    K:::::KN:::::::N       N::::::N   OO:::::::::OO   X:::::X       X:::::X                      ...(@@* .... .           &#//%@@&,.                       
+/*Z:::::::::::::::::ZK:::::::K    K:::::KN::::::::N      N::::::N OO:::::::::::::OO X:::::X       X:::::X                    ..*@@.........              .@#%%(%&@&..                    
+/*Z:::ZZZZZZZZ:::::Z K:::::::K   K::::::KN:::::::::N     N::::::NO:::::::OOO:::::::OX::::::X     X::::::X                   .*@( ........ .  .&@@@@.      .@%%%%%#&@@.                  
+/*ZZZZZ     Z:::::Z  KK::::::K  K:::::KKKN::::::::::N    N::::::NO::::::O   O::::::OXXX:::::X   X::::::XX                ...&@ ......... .  &.     .@      /@%%%%%%&@@#                  
+/*        Z:::::Z      K:::::K K:::::K   N:::::::::::N   N::::::NO:::::O     O:::::O   X:::::X X:::::X                   ..@( .......... .  &.     ,&      /@%%%%&&&&@@@.              
+/*       Z:::::Z       K::::::K:::::K    N:::::::N::::N  N::::::NO:::::O     O:::::O    X:::::X:::::X                   ..&% ...........     .@%(#@#      ,@%%%%&&&&&@@@%.               
+/*      Z:::::Z        K:::::::::::K     N::::::N N::::N N::::::NO:::::O     O:::::O     X:::::::::X                   ..,@ ............                 *@%%%&%&&&&&&@@@.               
+/*     Z:::::Z         K:::::::::::K     N::::::N  N::::N:::::::NO:::::O     O:::::O     X:::::::::X                  ..(@ .............             ,#@&&&&&&&&&&&&@@@@*               
+/*    Z:::::Z          K::::::K:::::K    N::::::N   N:::::::::::NO:::::O     O:::::O    X:::::X:::::X                   .*@..............  . ..,(%&@@&&&&&&&&&&&&&&&&@@@@,               
+/*   Z:::::Z           K:::::K K:::::K   N::::::N    N::::::::::NO:::::O     O:::::O   X:::::X X:::::X                 ...&#............. *@@&&&&&&&&&&&&&&&&&&&&@@&@@@@&                
+/*ZZZ:::::Z     ZZZZZKK::::::K  K:::::KKKN::::::N     N:::::::::NO::::::O   O::::::OXXX:::::X   X::::::XX               ...@/.......... *@@@@. ,@@.  &@&&&&&&@@@@@@@@@@@.               
+/*Z::::::ZZZZZZZZ:::ZK:::::::K   K::::::KN::::::N      N::::::::NO:::::::OOO:::::::OX::::::X     X::::::X               ....&#..........@@@, *@@&&&@% .@@@@@@@@@@@@@@@&                  
+/*Z:::::::::::::::::ZK:::::::K    K:::::KN::::::N       N:::::::N OO:::::::::::::OO X:::::X       X:::::X                ....*@.,......,@@@...@@@@@@&..%@@@@@@@@@@@@@/                   
+/*Z:::::::::::::::::ZK:::::::K    K:::::KN::::::N        N::::::N   OO:::::::::OO   X:::::X       X:::::X                   ...*@,,.....%@@@,.........%@@@@@@@@@@@@(                     
+/*ZZZZZZZZZZZZZZZZZZZKKKKKKKKK    KKKKKKKNNNNNNNN         NNNNNNN     OOOOOOOOO     XXXXXXX       XXXXXXX                      ...&@,....*@@@@@ ..,@@@@@@@@@@@@@&.                     
+/*                                                                                                                                   ....,(&@@&..,,,/@&#*. .                             
+/*                                                                                                                                    ......(&.,.,,/&@,.                                
+/*                                                                                                                                      .....,%*.,*@%                               
+/*                                                                                                                                    .#@@@&(&@*,,*@@%,..                               
+/*                                                                                                                                    .##,,,**$.,,*@@@@@%.                               
+/*                                                                                                                                     *(%%&&@(,,**@@@@@&                              
+/*                                                                                                                                      . .  .#@((@@(*,**                                
+/*                                                                                                                                             . (*. .                                   
+/*                                                                                                                                              .*/
+///* Copyright (C) 2025 - Renaud Dubois, Simon Masson - This file is part of ZKNOX project
+///* License: This software is licensed under MIT License
+///* This Code may be reused including this header, license and copyright notice.
+///* See LICENSE file at the root folder of the project.
+///* FILE: ZKNOX_falcon.sol
+///* Description: shake XOF function  tests
+/**
+ *
+ */
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.25;
 
 import "../src/ZKNOX_shake.sol";
 import {Test, console} from "forge-std/Test.sol";
 
-contract ZKNOX_DelegateTest is Test {
+contract ZKNOX_ShakeTest is Test {
     ZKNOX_shake oneal = new ZKNOX_shake();
 
     function test_F1600Zero() public view {
@@ -37,9 +76,21 @@ contract ZKNOX_DelegateTest is Test {
             0xEAF1FF7B5CECA249
         ];
 
-        uint64[25] memory res = oneal.F1600(zeroes);
+        uint64[25] memory res = oneal.F1600(zeroes); 
+ 
+
+        
         for (uint256 i = 0; i < 25; i++) {
             assertEq(res[i], TEST_F1600_ZERO[i]);
         }
     }
+
+    function test_update() public view{
+
+        bytes memory message=hex'B32D95B0B9AAD2A8816DE6D06D1F86008505BD8C14124F6E9A163B5A2ADE55F835D0EC3880EF50700D3B25E42CC0AF050CCD1BE5E555B23087E04D7BF9813622780C7313A1954F8740B6EE2D3F71F768DD417F520482BD3A08D4F222B4EE9DBD015447B33507DD50F3AB4247C5DE9A8ABD62A8DECEA01E3B87C8B927F5B08BEB37674C6F8E380C04';
+
+        
+
+    }
+
 }
